79acd908b999addaf0d14d39689d58db7eba33bb
Niles Rogoff <me@niles.xyz>
2018-06-23 11:31:36 +0000

    add link

--- generate.py
+++ generate.py
@@ -43,6 +43,7 @@ l = [
             , ["/dumb memes", "a","https://reddit.com/r/anime_irl" ]
             , ["/osu", "o","https://reddit.com/r/osugame" ]
             , ["/programming", "r","https://reddit.com/r/programming" ]
+            , ["/android", "n","https://reddit.com/r/android" ]
         ]]
         , ["/r?", "r", SEARCH, "https://reddit.com/r/%s", "reddit.com/r/"]
         , ["/u?", "u", SEARCH, "https://reddit.com/u/%s", "reddit.com/u/"]
